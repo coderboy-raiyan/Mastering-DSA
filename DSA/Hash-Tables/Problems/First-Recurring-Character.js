014de5e4fc294b29db3fbe1e7612e37d2f639d8f
Raiyan <tajkierhaque@gmail.com>
2022-06-25 20:54:00 +0600

    firstRecurringCharacter hash table mathod added

--- DSA/Hash-Tables/Problems/First-Recurring-Character.js
+++ DSA/Hash-Tables/Problems/First-Recurring-Character.js
@@ -12,9 +12,52 @@
 // [2,5,5,2,3,5,1,2,4]
 // return 5 because the pairs are before 2,2
 
-const array = [2, 5, 5, 3, 5, 2, 2, 3, 5, 1, 2, 4];
+const array = [2, 5, 5, 2, 3, 5, 1, 2, 4];
+
+/*
+  This "firstRecurringCharacter" function will give a different result rather then the 2 qnd 3 function 
+
+  * The complexity is : O(n ^ 2)
+  * The space complexity is : O(1)
+
+*/
 
 function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+} // O(n ^ 2)
+
+/*
+  This "firstRecurringCharacter2" and "firstRecurringCharacter3" are going to give us the same result but they have different complexities
+
+  firstRecurringCharacter2 : -
+  * The complexity is : O(n)
+  * The space complexity is : O(n)
+  * 
+  firstRecurringCharacter3 : -
+  * The complexity is : O(n)
+  * The space complexity is : O(1)
+
+*/
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = input[i];
+    }
+  }
+  return undefined;
+} // O(n)
+
+function firstRecurringCharacter3(input) {
   let curr = input[0];
   for (let i = 0; i < input.length; i++) {
     if (curr === input[i + 1]) {
@@ -23,5 +66,7 @@ function firstRecurringCharacter(input) {
       curr = input[i + 1];
     }
   }
-}
-console.log(firstRecurringCharacter(array));
+  return undefined;
+} // O(n)
+
+console.log(firstRecurringCharacter3(array));
