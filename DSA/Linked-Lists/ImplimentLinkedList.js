b47c9261430276851dd27a7cfadf6dc74ecfd802
Raiyan <tajkierhaque@gmail.com>
2022-07-05 01:06:35 +0600

    linkedlist implimentaion done

--- DSA/Linked-Lists/ImplimentLinkedList.js
+++ DSA/Linked-Lists/ImplimentLinkedList.js
@@ -38,10 +38,75 @@ class MyLinkedList {
     }
     return array;
   }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    if (index === 0) {
+      return this.prepend(value);
+    }
+
+    const newNode = new Node(value);
+    const leader = this.findTheLeader(index - 1);
+    const holdPointer = this.findHoldPointer(index);
+    leader.next = newNode;
+    newNode.next = holdPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  remove(index) {
+    if (index === this.length) {
+      const leader = this.findTheLeader(index - 2);
+      (leader.next = null), (this.tail = leader);
+      this.length--;
+
+      return this.printList();
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+
+      return this.printList();
+    }
+
+    if (index !== 0 || index !== this.length) {
+      const leader = this.findTheLeader(index - 1);
+      const unwantedNode = leader.next;
+      leader.next = unwantedNode.next;
+      this.length--;
+      return this.printList();
+    }
+  }
+
+  findTheLeader(index) {
+    let counter = 0;
+    let currentHead = this.head;
+    while (counter !== index) {
+      currentHead = currentHead.next;
+      counter++;
+    }
+    return currentHead;
+  }
+  findHoldPointer(index) {
+    let counter = 0;
+    let currentHead = this.head;
+    while (counter !== index) {
+      currentHead = currentHead.next;
+      counter++;
+    }
+    return currentHead;
+  }
 }
+
+// [1,24,6,7]
 
 const myLinkedList = new MyLinkedList(5);
 
 console.log(myLinkedList.append(10));
+console.log(myLinkedList.append(20));
+console.log(myLinkedList.append(30));
 console.log(myLinkedList.prepend(15));
-console.log(myLinkedList.printList());
+console.log(myLinkedList.insert(2, 33));
+console.log(myLinkedList.remove(myLinkedList.length));
+console.log(myLinkedList.remove(0));
